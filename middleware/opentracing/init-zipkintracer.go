3f6d3156b0fb21bfcf21d74724ae29f4bb6280ca
waro163 <waro163@163.com>
2020-10-19 12:22:19 +0800

    modify the zipkin middleware serve function, remove the ZipkinSpan

--- middleware/opentracing/init-zipkintracer.go
+++ middleware/opentracing/init-zipkintracer.go
@@ -9,10 +9,20 @@ import (
 
 // GlobalTracer is zipkin Tracer
 var GlobalTracer *zipkin.Tracer
+var zipkinConfig Config
+
+// Config is zipkin Global Tracer configuration
+type Config struct {
+	// Debug configure zipkin output
+	// default false
+	Debug bool
+}
 
 // InitZipkinTracer is for initilize zipkin global Tracer function
 func InitZipkinTracer(debug bool) *zipkin.Tracer {
 	var err error
+	// set up zipkin config
+	zipkinConfig = Config{Debug: debug}
 	if debug {
 		// set up a span reporter
 		// this reporter can be replaced with amqp, http, kafka, recorder...
--- middleware/opentracing/opentracing.go
+++ middleware/opentracing/opentracing.go
@@ -7,7 +7,6 @@ import (
 	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
 
-	zipkin "github.com/openzipkin/zipkin-go"
 	"github.com/openzipkin/zipkin-go/model"
 	"github.com/openzipkin/zipkin-go/propagation/b3"
 )
@@ -15,7 +14,11 @@ import (
 // ActiveSpan is global opentraing parent span, will be initialized in opentracing Serve function
 // restclient use it to generate child span
 var ActiveSpan opentracing.Span
-var ZipkinSpan zipkin.Span
+
+// var ZipkinSpan zipkin.Span
+
+// ZipkinSpanContext is span context from income request header
+var ZipkinSpanContext model.SpanContext
 
 // Serve is opentracing middleware function
 // parameter operationName is a operaion name, usually named it service name
@@ -41,19 +44,31 @@ func Serve(operationName string) func(context.Context) {
 
 func ZipkinServe(operationName string) func(context.Context) {
 	return func(ctx context.Context) {
-		var spancontext model.SpanContext
 		sc := GlobalTracer.Extract(b3.ExtractHTTP(ctx.Request()))
 		if sc.Err != nil {
 			log.Printf("global tracer extract span context not found: %v\n", sc.Err)
 		}
-		// if sc.Err is not nil, Parent(sc) maybe panic, so setting spancontext to empty struct in this condition
-		spancontext = sc
-		ZipkinSpan = GlobalTracer.StartSpan(operationName,
-			zipkin.Kind(model.Server),
-			zipkin.Parent(spancontext),
-		)
-		defer ZipkinSpan.Finish()
+		ZipkinSpanContext = sc
+		if zipkinConfig.Debug {
+			output(ZipkinSpanContext)
+		}
+		// ZipkinSpan = GlobalTracer.StartSpan(operationName,
+		// 	// zipkin.Kind(model.Server),
+		// 	zipkin.Parent(sc),
+		// )
+		// defer ZipkinSpan.Finish()
+
 		// todo: add tag to ActiveSpan
 		ctx.Next()
 	}
+}
+
+func output(sc model.SpanContext) {
+	log.Printf("------------------receive request header opentracing inf------------------")
+	log.Printf("TraceID: %s\n", sc.TraceID.String())
+	if sc.ParentID != nil {
+		log.Printf("ParentID: %s\n", (*sc.ParentID).String())
+	}
+	log.Printf("ID: %s\n", sc.ID.String())
+	log.Printf("------------------ end ------------------")
 }
