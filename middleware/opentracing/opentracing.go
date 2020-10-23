35806a4b38cdf424b6be9b25f9f3bd7be8922a10
waro163 <waro163@163.com>
2020-10-23 15:47:01 +0800

    fix the bug in opentracing midddleware about "fatal error: concurrent map read and map write"

--- middleware/opentracing/opentracing.go
+++ middleware/opentracing/opentracing.go
@@ -1,89 +1,56 @@
 package opentracing
 
 import (
-	"log"
 	"strings"
+	"sync"
 
 	"github.com/kataras/iris/v12/context"
-
-	zipkin "github.com/openzipkin/zipkin-go"
-	"github.com/openzipkin/zipkin-go/model"
-	"github.com/openzipkin/zipkin-go/propagation/b3"
 )
 
-// ZipkinSpan is span
-// if the span-context that extract from incoming request header is not nil, ZipkinSpan will be child span of incoming
-// else will be root span
-var ZipkinSpan zipkin.Span
-
-// OpentracingInf saving opentracing information that incoming request header
-// usually contain "x-request-id", "x-b3-traceid", "x-b3-spanid", "x-b3-parentspanid", "x-b3-sampled"...
-// for having installed envoy kubernetes pods, only transmit the header to next pod is enough
-var OpentracingInf map[string][]string
-
-func ZipkinServe(operationName string) func(context.Context) {
-	return func(ctx context.Context) {
-		if GlobalTracer == nil {
-			log.Printf("[WARNNING] Before using Zipkin opentracing middleware, PLZ init InitZipkinTracer, otherwise it do NOT work!!!\n")
-			ctx.Next()
-			return
-		}
-		sc := GlobalTracer.Extract(b3.ExtractHTTP(ctx.Request()))
-		if sc.Err != nil {
-			log.Printf("global tracer extract span context not found: %v\n", sc.Err)
-		}
-
-		ZipkinSpan = GlobalTracer.StartSpan(operationName,
-			// zipkin.Kind(model.Server),
-			zipkin.Parent(sc),
-		)
-		// defer ZipkinSpan.Finish()
-
-		// todo: add tag to ZipkinSpan
-		if zipkinConfig.Debug {
-			log.Printf("------------------ origin span context ------------------")
-			output(sc)
-			log.Printf("------------------ generate span context ------------------")
-			output(ZipkinSpan.Context())
-		}
-		ctx.Next()
-	}
+// OpentracingHeader is saving opentracing b3 header data
+// field Headers is map[string][]string type
+type OpentracingHeader struct {
+	sync.RWMutex
+	Headers interface{}
 }
 
-// ------------Inject zipkin b3 header to reqeust header----------
-// if opentracing.GlobalTracer != nil && opentracing.ZipkinSpan != nil {
-// 	var operationName string
-// 	operationName, ok = kwarg["operationname"]
-// 	if !ok {
-// 		operationName = destURL
-// 	}
+// OpentracingInf global variable to transmit opentracing b3 header data
+var OpentracingInf = new(OpentracingHeader)
 
-// 	appSpan := opentracing.GlobalTracer.StartSpan(operationName,
-// 		zipkin.Parent(opentracing.ZipkinSpan.Context()),
-// 	)
-// 	defer appSpan.Finish()
-// 	zipkin.TagHTTPMethod.Set(appSpan, req.Method)
-// 	zipkin.TagHTTPPath.Set(appSpan, req.URL.Path)
-// 	_ = b3.InjectHTTP(req, b3.WithSingleAndMultiHeader())(appSpan.Context())
-// 	// log.Printf("send %s header is %#v\n", destURL, req.Header)
-// }
+// SetHeaders set OpentracingHeader Headers function
+func (oh *OpentracingHeader) SetHeaders(data interface{}) {
+	oh.Lock()
+	oh.Headers = data
+	oh.Unlock()
+}
 
-func output(sc model.SpanContext) {
-	log.Printf("TraceID: %s\n", sc.TraceID.String())
-	if sc.ParentID != nil {
-		log.Printf("ParentID: %s\n", (*sc.ParentID).String())
-	}
-	log.Printf("ID: %s\n", sc.ID.String())
+// GetHeaders get OpentracingHeader Headers function
+func (oh *OpentracingHeader) GetHeaders() interface{} {
+	oh.RLock()
+	data := oh.Headers
+	oh.RUnlock()
+	return data
 }
 
 // Serve is for saving incoming request header of b3
 func Serve(ctx context.Context) {
-	OpentracingInf = make(map[string][]string)
+	data := make(map[string][]string)
 	headers := ctx.Request().Header
 	for key, value := range headers {
 		if strings.HasPrefix(key, "X-") || strings.HasPrefix(key, "x-") {
-			OpentracingInf[key] = value
+			data[key] = value
 		}
 	}
+	OpentracingInf.SetHeaders(data)
 	ctx.Next()
 }
+
+// Inject b3 header to req header
+// b3headers, ok := opentracing.OpentracingInf.GetHeaders().(map[string][]string)
+// if ok {
+// 	for key, value := range b3headers {
+// 		if len(value) > 0 {
+// 			req.Header.Set(key, value[0])
+// 		}
+// 	}
+// }
