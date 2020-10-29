57b15ef4a232eb092244015d52fb5b6cc6decd19
waro163 <waro163@163.com>
2020-10-29 14:01:32 +0800

    fix the bug when apipath contain space character in restclient package

--- restclient/build-url.go
+++ restclient/build-url.go
@@ -2,8 +2,17 @@ package restclient
 
 import (
 	"fmt"
+	"log"
+	"net/url"
 )
 
 func buildURL(protocol string, address string, apiPath string) string {
-	return fmt.Sprintf("%s://%s%s", protocol, address, apiPath)
+	tempURL := fmt.Sprintf("%s://%s%s", protocol, address, apiPath)
+	destURL, err := url.Parse(tempURL)
+	if err != nil {
+		log.Printf("parse url: %s occur error: %s\n", tempURL, err)
+		return tempURL
+	}
+	destURL.RawQuery = destURL.Query().Encode()
+	return destURL.String()
 }
