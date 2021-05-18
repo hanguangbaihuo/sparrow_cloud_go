c22eac58c4ed7e02aac7fa66f5fcbdb8eb58b16b
waro163 <waro163@163.com>
2021-05-18 14:36:58 +0800

    add testcase for jwt middleware

--- middleware/jwt/jwt_test.go
+++ middleware/jwt/jwt_test.go
@@ -6,58 +6,95 @@ package jwt
 // 我们需要修改默认的 TokenExtractor
 
 import (
-    "github.com/kataras/iris/v12"
-    "github.com/kataras/iris/v12/context"
-    "github.com/kataras/iris/v12/httptest"
-    "testing"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/context"
+	"github.com/kataras/iris/v12/httptest"
 )
 
 type Response struct {
-    Text string `json:"text"`
+	Text string `json:"text"`
 }
 
+var (
+	app       = iris.New()
+	jwtSecret = []byte("My JWTSecret")
+)
+
 func TestBasicJwt(t *testing.T) {
-    var (
-        app = iris.New()
-        j   = New(Config{
-            ValidationKeyGetter: func(token *Token) (interface{}, error) {
-                return []byte("My Secret"), nil
-            },
-            SigningMethod: SigningMethodHS256,
-        })
-    )
 
-    securedPingHandler := func(ctx context.Context) {
-        userToken := j.Get(ctx)
-        var claimTestedValue string
-        if claims, ok := userToken.Claims.(MapClaims); ok && userToken.Valid {
-            claimTestedValue = claims["foo"].(string)
-        } else {
-            claimTestedValue = "Claims Failed"
-        }
+	os.Setenv("JWT_SECRET", string(jwtSecret))
 
-        response := Response{"Iauthenticated" + claimTestedValue}
-        // get the *Token which contains user information using:
-        // user:= j.Get(ctx) or ctx.Values().Get("jwt").(*Token)
+	handlePing := func(ctx context.Context) {
+		ctx.JSON(context.Map{"message": "pong"})
+	}
 
-        ctx.JSON(response)
-    }
+	app.Get("/secured/ping", AutoServe, handlePing)
+	e := httptest.New(t, app)
 
-    app.Get("/secured/ping", j.Serve, securedPingHandler)
-    e := httptest.New(t, app)
+	// test normal token
+	token := NewTokenWithClaims(SigningMethodHS256, MapClaims{
+		"exp": time.Now().Unix() + 100,
+		"uid": "abc123",
+	})
+	tokenString, _ := token.SignedString(jwtSecret)
 
-    e.GET("/secured/ping").Expect().Status(iris.StatusUnauthorized)
+	e.GET("/secured/ping").WithHeader("Authorization", "Token "+tokenString).
+		Expect().Status(iris.StatusOK)
+}
+func TestEmptyToken(t *testing.T) {
+	os.Setenv("JWT_SECRET", string(jwtSecret))
 
-    // Create a new token object, specifying signing method and the claims
-    // you would like it to contain.
-    token := NewTokenWithClaims(SigningMethodHS256, MapClaims{
-        "foo": "bar",
-    })
+	handlePing := func(ctx context.Context) {
+		ctx.JSON(context.Map{"message": "pong"})
+	}
 
-    // Sign and get the complete encoded token as a string using the secret
-    tokenString, _ := token.SignedString([]byte("My Secret"))
+	app.Get("/secured/ping", AutoServe, handlePing)
+	e := httptest.New(t, app)
 
-    // e.GET("/secured/ping").WithHeader("Authorization", "Bearer "+tokenString).
-    e.GET("/secured/ping").WithHeader("Authorization", "Token "+tokenString).
-        Expect().Status(iris.StatusOK).Body().Contains("Iauthenticated").Contains("bar")
+	// test empty jwt token header
+	e.GET("/secured/ping").Expect().Status(iris.StatusOK)
+}
+
+func TestExpireToken(t *testing.T) {
+	os.Setenv("JWT_SECRET", string(jwtSecret))
+
+	handlePing := func(ctx context.Context) {
+		ctx.JSON(context.Map{"message": "pong"})
+	}
+
+	app.Get("/secured/ping", AutoServe, handlePing)
+	e := httptest.New(t, app)
+
+	token := NewTokenWithClaims(SigningMethodHS256, MapClaims{
+		"exp": time.Now().Unix() - 100,
+		"uid": "abc123",
+	})
+	tokenString, _ := token.SignedString(jwtSecret)
+
+	e.GET("/secured/ping").WithHeader("Authorization", "Token "+tokenString).
+		Expect().Status(iris.StatusUnauthorized).Body().Contains("expired")
+}
+
+func TestInvalidToken(t *testing.T) {
+	os.Setenv("JWT_SECRET", string(jwtSecret))
+
+	handlePing := func(ctx context.Context) {
+		ctx.JSON(context.Map{"message": "pong"})
+	}
+
+	app.Get("/secured/ping", AutoServe, handlePing)
+	e := httptest.New(t, app)
+
+	token := NewTokenWithClaims(SigningMethodHS256, MapClaims{
+		"exp": time.Now().Unix() + 500,
+		"uid": "abc123",
+	})
+	tokenString, _ := token.SignedString([]byte("wrongjwtSecret"))
+
+	e.GET("/secured/ping").WithHeader("Authorization", "Token "+tokenString).
+		Expect().Status(iris.StatusUnauthorized).Body().Contains("invalid")
 }
