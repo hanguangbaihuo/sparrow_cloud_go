eda866db9c68e39ee51389815713d2551efa3064
waro163 <waro163@163.com>
2021-06-07 16:03:47 +0800

    add GetSecret function to jwt middleware

---
+++ middleware/jwt/secret.go
@@ -0,0 +1,28 @@
+package jwt
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+var RsaPublicSecret []byte
+
+func init() {
+	RsaPublicKeyPath := os.Getenv("PUBLIC_KEY_PATH")
+	var err error
+	RsaPublicSecret, err = ioutil.ReadFile(RsaPublicKeyPath)
+	if err != nil {
+		panic("[JWT] read rsa public file err")
+	}
+}
+
+func GetSecret(algorithm string) ([]byte, error) {
+	if algorithm == "HS256" {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	} else if algorithm == "RS256" {
+		return RsaPublicSecret, nil
+	} else {
+		return []byte(""), fmt.Errorf("[JWT] wrong signing method %s", algorithm)
+	}
+}
