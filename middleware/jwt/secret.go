dbf8d63f311e7f41983b25573e487f1ae8639bb0
waro163 <waro163@163.com>
2021-06-08 15:04:06 +0800

    fix the bug in GetSecret function in jwt middleware

--- middleware/jwt/secret.go
+++ middleware/jwt/secret.go
@@ -1,23 +1,29 @@
 package jwt
 
 import (
+	"crypto/rsa"
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
-var RsaPublicSecret []byte
+var RsaPublicSecret *rsa.PublicKey
 
 func init() {
 	RsaPublicKeyPath := os.Getenv("PUBLIC_KEY_PATH")
-	var err error
-	RsaPublicSecret, err = ioutil.ReadFile(RsaPublicKeyPath)
+	RsaPublicKey, err := ioutil.ReadFile(RsaPublicKeyPath)
 	if err != nil {
 		panic(fmt.Sprintf("[JWT] read rsa public file err: %s", err))
 	}
+	RsaPublicSecret, err = jwt.ParseRSAPublicKeyFromPEM(RsaPublicKey)
+	if err != nil {
+		panic(fmt.Sprintf("[JWT] convert rsa public key to rsa struct error: %s", err))
+	}
 }
 
-func GetSecret(algorithm string) ([]byte, error) {
+func GetSecret(algorithm string) (interface{}, error) {
 	if algorithm == "HS256" {
 		return []byte(os.Getenv("JWT_SECRET")), nil
 	} else if algorithm == "RS256" {
