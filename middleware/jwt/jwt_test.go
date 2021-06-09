25ec9a18653496b1a1e61926966113a9e5d15ec2
waro163 <waro163@163.com>
2021-06-09 15:21:52 +0800

    add testcase for jwt generate X-Jwt-Payload header

--- middleware/jwt/jwt_test.go
+++ middleware/jwt/jwt_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/kataras/iris/v12/httptest"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/hanguangbaihuo/sparrow_cloud_go/middleware/auth"
 )
 
 var (
@@ -138,16 +139,46 @@ func TestInvalidToken(t *testing.T) {
 		Expect().Status(iris.StatusUnauthorized).Body().Contains("invalid")
 
 	// test rs256 token
-	// token = NewTokenWithClaims(SigningMethodRS256, MapClaims{
-	// 	"exp": time.Now().Unix() + 500,
-	// 	"uid": "abc123",
-	// })
-	// tokenString, err = token.SignedString([]byte("wrongjwtSecret"))
-	// if err != nil {
-	// 	t.Errorf("signed rs256 token error: %s\n", err)
-	// }
 	invalidRsaToken := "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOiIxMjM0YWJjIiwiZXhwIjoxNzIyMjAwMzE2LCJpYXQiOjE2MjIxOTMxMTYsImFwcF9pZCI6ImNvcmUifQ.test"
 
 	e.GET("/secured/ping").WithHeader("Authorization", "Tokentest "+invalidRsaToken).
 		Expect().Status(iris.StatusUnauthorized)
+}
+
+// test X-Jwt-Payload header the jwt middleware generate
+func TestPayloadHeader(t *testing.T) {
+	var app = iris.New()
+	os.Setenv("JWT_SECRET", string(jwtSecret))
+
+	handlePing := func(ctx context.Context) {
+		ctx.JSON(context.Map{"message": "pong"})
+	}
+
+	app.Get("/secured/ping", AutoServe, auth.IsAuthenticated, handlePing)
+	e := httptest.New(t, app)
+	//test hs256 token header
+	token := NewTokenWithClaims(SigningMethodHS256, MapClaims{
+		"exp": time.Now().Unix() + 100,
+		"uid": "abc123",
+	})
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		t.Errorf("signed hs256 token error: %s\n", err)
+	}
+
+	e.GET("/secured/ping").WithHeader("Authorization", "Tokentest "+tokenString).
+		Expect().Status(iris.StatusOK).Body().Contains("pong")
+
+	// test rs256 token header
+	token = NewTokenWithClaims(SigningMethodRS256, MapClaims{
+		"exp": time.Now().Unix() + 100,
+		"uid": "abc123",
+	})
+	tokenString, err = token.SignedString(rsaPrivateSecret)
+	if err != nil {
+		t.Errorf("signed rs256 token error: %s\n", err)
+	}
+
+	e.GET("/secured/ping").WithHeader("Authorization", "Tokentest "+tokenString).
+		Expect().Status(iris.StatusOK)
 }
